504af2589900d6e639700fefc9db2f32901c880e
Sergey Egorenkov <segorenkov@Sergeys-MacBook-Pro.local>
2019-07-03 19:26:46 +0300

    Set Firebase

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ import React, {Component} from 'react';
 import { Provider } from 'react-redux';
 import store from '@redux';
 import AppContainer from '@navigation';
+import firebase from 'react-native-firebase'
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentDidMount() {
+    // firebase
+    //   .auth()
+    //   .signInWithEmailAndPassword('egsesmail2019@gmail.com', 'qweASD123')
+    //   .then(res => console.log('Res: ', res))
+    //   .catch(error => console.log('Error: ', error))
+  }
   render() {
     return <Provider store={store}>
       <AppContainer />
